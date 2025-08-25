d52ada5d7828849fb8b999b7b7c07646ef541681
Karina Meira <karinameira03@hotmail.com>
2025-08-25 18:13:02 +0200

    Code improvements

--- curso-em-video/javascript/ex05/ex05.js
+++ curso-em-video/javascript/ex05/ex05.js
@@ -7,17 +7,24 @@ function contar() {
     // Validação de campos vazios
     if (document.querySelector('#inicio').value.length == 0 || document.querySelector('#fim').value.length == 0 || document.querySelector('#passo').value.length == 0) {
         resp.innerHTML = '[ERRO] Faltam dados!'
-        return
-    }
+    } else {
+        resp.innerHTML = 'Contando: <br>'
+        // Validção se o passo for = 0
+        if (p <= 0) {
+            alert('Passo inválido! Considerando passo = 1')
+            p = 1
+        }
 
-    // Validção se o passo for = 0
-    if (p <= 0) {
-        alert('Passo inválido! Considerando passo = 1')
-        p = 1
-    }
-
-    for (let c = i; c <= f; c += p) {
-        resp.innerHTML += `-> ${c}`
-    }
-    
+        if (i < f) {
+            // Contagem crescente
+            for (let c = i; c <= f; c += p) {
+                resp.innerHTML += `-> ${c}`
+            }
+        } else {
+            // Contagem descresente
+            for (let c = i; c >= f; c -= p) {
+                resp.innerHTML += `-> ${c}`
+            }
+        }
+    }  
 }
