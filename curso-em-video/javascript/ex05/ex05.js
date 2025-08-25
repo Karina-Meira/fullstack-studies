672d768301b2091055e0df5f2ef0ec768f66596a
Karina Meira <karinameira03@hotmail.com>
2025-08-25 17:09:11 +0200

    Update exercise 05

--- curso-em-video/javascript/ex05/ex05.js
+++ curso-em-video/javascript/ex05/ex05.js
@@ -4,10 +4,20 @@ function contar() {
     let p = Number(document.querySelector('#passo').value)
     let resp = document.querySelector('#info')
 
-    for (i; i < f; i = i++) {
-        resp.innerHTML = i
+    // Validação de campos vazios
+    if (document.querySelector('#inicio').value.length == 0 || document.querySelector('#fim').value.length == 0 || document.querySelector('#passo').value.length == 0) {
+        resp.innerHTML = '[ERRO] Faltam dados!'
+        return
+    }
 
+    // Validção se o passo for = 0
+    if (p <= 0) {
+        alert('Passo inválido! Considerando passo = 1')
+        p = 1
+    }
 
+    for (let c = i; c <= f; c += p) {
+        resp.innerHTML += `-> ${c}`
+    }
     
-    resp.innerHTML = ("Fim!")
 }
