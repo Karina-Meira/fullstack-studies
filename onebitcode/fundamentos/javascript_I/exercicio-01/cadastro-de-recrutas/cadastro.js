bba879745f25089447b4538977433f7e283cad7c
Karina Meira <karinameira03@hotmail.com>
2025-08-28 17:58:14 +0200

    Solve two exercices from JavaScript I

---
+++ onebitcode/fundamentos/javascript_I/exercicio-01/cadastro-de-recrutas/cadastro.js
@@ -0,0 +1,7 @@
+let nome = prompt('Digite seu nome: ')
+let sobrenome = prompt('Digite seu sobrenome: ')
+let campo_de_estudo = prompt('Digite seu campo de estudo: ')
+let nasc = parseFloat(prompt('Digite o ano do seu nascimento'))
+
+alert(`Nome do recruta: ${nome} ${sobrenome}\n` + `Campo de estudo do recruta: ${campo_de_estudo}\n` + `Idade do recruta: ${2025 - nasc}`
+)
---
+++ onebitcode/fundamentos/javascript_I/exercicio-01/calculadora-4-operacoes/calculadora4.js
@@ -0,0 +1,10 @@
+let num1 = parseFloat(prompt('Digite o primeiro número:'))
+let num2 = parseFloat(prompt('Digite o segundo número:'))
+
+alert(
+    `Som: ${num1 + num2}\n` + 
+    `Subtração: ${num1 - num2}\n` + 
+    `Multipicação: ${num1 * num2}\n` + 
+    `Divisão: ${num1 / num2}`
+)
+
